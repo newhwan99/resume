186af64ddf13c48ff5ff43d883e97688d5ba43fa
newhwan99 <newhwan99@gamil.com>
2024-08-14 00:30:57 +0900

    Updates

---
+++ precache-manifest.cbd635ca3af1bddf91a626817dde5057.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40e97764c19a93b26531ea1557ac8ef",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "7c74b5b152c4dd21aa4b",
+    "url": "/resume/static/css/main.293d3188.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/resume/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/resume/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "7c74b5b152c4dd21aa4b",
+    "url": "/resume/static/js/main.4b8b6eab.chunk.js"
+  },
+  {
+    "revision": "70f9704cabd9049084c6",
+    "url": "/resume/static/js/runtime~main.b6537859.js"
+  },
+  {
+    "revision": "cd12b96f9d86e99a36c1c594fccbca8a",
+    "url": "/resume/static/media/profileImage.cd12b96f.png"
+  }
+]);
